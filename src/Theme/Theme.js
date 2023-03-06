0a82ea5e696754bf6eb0e3391968e8e9025c79d9
antu <antusaha970@gmail.com>
2023-03-07 01:02:29 +0600

    added images and custom theme

---
+++ src/Theme/Theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#19D3AE",
+        },
+        secondary: {
+            main: "#3A4256",
+        },
+    }
+})
---
+++ src/components/TopLanding/TopLanding.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopLanding = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TopLanding;
